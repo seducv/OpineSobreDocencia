e5f333b370a59bf4256d2a6d29c7c63e96c4bac2
Miguel Angel Magdalena <miguelangelmagdalena@gmail.com>
2020-02-15 11:08:42 -0400

    Colocando descripcion de los Surveys como nullable

--- database/migrations/2017_11_19_013619_create_surveys_table.php
+++ database/migrations/2017_11_19_013619_create_surveys_table.php
@@ -15,7 +15,7 @@ class CreateSurveysTable extends Migration
         Schema::create('surveys', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
